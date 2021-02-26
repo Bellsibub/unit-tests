6ac238050bd60e862f619b0ee139fe02922cf7da
Bellsibub <isabella.billgren@gmail.com>
2021-02-26 12:16:35 +0100

    added eslint & fixed all req. tests

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,22 @@
 export const hackerSpeak = (words) => {
-  return words
+  const re = /a|e|i|o|s/gi
+  return words.replace(re, (match) => {
+    switch (match) {
+      case "a":
+      case "A":
+        return 4
+      case "e":
+      case "E":
+        return 3
+      case "i":
+      case "I":
+        return 1
+      case "o":
+      case "O":
+        return 0
+      case "s":
+      case "S":
+        return 5
+    }
+  })
 }
